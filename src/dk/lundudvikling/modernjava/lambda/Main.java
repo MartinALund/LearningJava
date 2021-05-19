055a3c41569405306d969a9ef8b16061b1d63162
Martin Lund <privatmartinlund@gmail.com>
2021-05-19 21:13:10 +0200

    Added class

---
+++ src/dk/lundudvikling/modernjava/lambda/Main.java
@@ -0,0 +1,14 @@
+package dk.lundudvikling.modernjava.lambda;
+
+import dk.lundudvikling.models.Person;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Person person1 = new Person("Martin", "Lund", 29);
+        Person person2 = new Person("Test", "Testesen", 30);
+
+
+    }
+}
